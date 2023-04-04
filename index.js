96efeff19834dc1bddd2d552c2d6ad7829759947
Sudarshan Patil <91423165+OptifySudarshanPatil@users.noreply.github.com>
2023-04-04 15:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 const http = require('http');
 const url = require('url');
-
+var count = 0;
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
   
@@ -16,11 +16,12 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       const data = query;
       const result = data.result === 'pass' ? 'success' : 'failure';
+      console.log(`Count : ${count++}`);
       console.log(`Data 1: ${data.d1}`);
       console.log(`Data 2: ${data.d2}`);
-      console.log(`Result: ${result}`);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('202\n');
+      console.log(`Result: ${result}\n`);
+      res.writeHead(202, {'Content-Type': 'text/plain'});
+      res.end('202');
     });
   } else {
     res.writeHead(404, {'Content-Type': 'text/plain'});
